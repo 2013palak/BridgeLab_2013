f52e385f70b025c4834d0a26122b5e3044c3eb7a
@2013palak <palak2013.be22@chitkara.edu.in>
2025-07-09 10:57:12 +0530

    code

---
+++ code/src/FirstProgram.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class FirstProgram {
+        public static void main(String[] args)
+        {
+            System.out.println("hello world");
+            System.out.println(args[0]);
+
+        }
+    }
+
